98d758f3b023fb449594253b78000fea6e0c22d5
Caleb Wood <Calebwjwood60@gmail.com>
2023-09-13 17:19:28 -0500

    fixed all route mistakes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,8 +8,18 @@ router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findAll()
+    const productData = await Product.findAll({
+    // include: [{  }]
 
+
+
+
+
+
+
+
+    
+  })
     res.status(200).json(productData)
   } catch (err) {
     res.status(500).json(err.message)
@@ -21,7 +31,19 @@ router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findByPk(req.body.params, {
+    const productData = await Product.findByPk(req.params.id, {
+      // include: [{  }]
+
+
+
+
+
+
+
+
+
+
+
 
     })
     if (!productData) {
@@ -109,9 +131,19 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
-  
+  try {
+    const productData = await Product.destroy({
+      where: {id: req.params.id}
+    })
+    if (!productData) {
+      res.status(400).json({message: 'no Product at that id'})
+    }
+    res.status(200).json(productData)
+  } catch (err) {
+    res.status(500).json(err.message)
+  }
 });
 
 module.exports = router;
